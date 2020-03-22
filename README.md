330be10d2ec662e79978a1503f0b1a2bae1f6660
Matej Mrnjec <matej.mrnjec@hotmail.com>
2020-03-23 00:35:31 +0100

    Init Project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gis
 School Project for GIS Lecture
+
+http://duspviz.mit.edu/web-map-workshop/leaflet_nodejs_postgis/
---
+++ client/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+import L from 'leaflet'
+export default {
+  name: 'App',
+  mounted() {
+      this.$nextTick(() => {
+        this.initMap();
+      });
+    },
+    methods: {
+        initMap: function(){
+            // Create variable to hold map element, give initial settings to map
+            var map = L.map('map',{ center: [42.375562, -71.106870], zoom: 14});
+            // Add OpenStreetMap tile layer to map element
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+              attribution: '© OpenStreetMap'
+            }).addTo(map);
+        }
+    }
+}
+</script>
+
+<style>
+@import '../node_modules/leaflet/dist/leaflet.css';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#map { 
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+ }
+</style>
